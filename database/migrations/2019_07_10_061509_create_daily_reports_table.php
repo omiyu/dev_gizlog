7744a98163978f47c82441506727c672f2e9c2af
omiyu <o1m.hello@icloud.com>
2019-07-10 16:45:16 +0900

    daily_reportsテーブルの作成

---
+++ database/migrations/2019_07_10_061509_create_daily_reports_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDailyReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('daily_reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("user_id");
+            $table->string("title");
+            $table->string("content");
+            $table->timestamp("reporting_time")->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('daily_reports');
+    }
+}
