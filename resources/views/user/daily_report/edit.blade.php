1c68ad66ae8269f6cf2a95ea50d14b7c5ffe0fb8
omiyu <o1m.hello@icloud.com>
2019-07-17 15:31:31 +0900

    コメントアウトの削除

--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -35,19 +35,3 @@
 </div>
 
 @endsection
-
-<!-- フォームタグの中身
-<input class="form-control" name="user_id" type="hidden" value="4">
-      <div class="form-group form-size-small">
-        <input class="form-control" name="reporting_time" type="date">
-      <span class="help-block"></span>
-      </div>
-      <div class="form-group">
-        <input class="form-control" placeholder="Title" name="title" type="text">
-      <span class="help-block"></span>
-      </div>
-      <div class="form-group">
-        <textarea class="form-control" placeholder="本文" name="contents" cols="50" rows="10">本文</textarea>
-      <span class="help-block"></span>
-      </div>
-      <button type="submit" class="btn btn-success pull-right">Update</button> -->
