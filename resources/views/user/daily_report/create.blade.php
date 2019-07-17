52e6ccde174bcfd12ded554831c17eaa87151514
omiyu <o1m.hello@icloud.com>
2019-07-17 21:22:57 +0900

    inputタグ(type=hidden)の削除とerrorメッセージ表示のコードの修正

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -4,30 +4,21 @@
 <h2 class="brand-header">日報作成</h2>
 <div class="main-wrap">
   {!! Form::open(['route' => 'daily_report.store',  'method' => 'POST', 'class' => 'container']) !!}
-    {!! Form::input('hidden', 'user_id', null, ['class' => 'form-control']) !!}
-      <div class="form-group form-size-small">
-    {!! Form::input('date', 'reporting_time', date('Y-m-d'), ['class' => 'form-control']) !!}
-    <span class="help-block"></span>
+    <div class="form-group form-size-small  ">
+      {!! Form::input('date', 'reporting_time', date('Y-m-d'), ['class' => 'form-control']) !!}
+      <span class="help-block"></span>
     </div>
-    <div class="form-group">
-    {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
-      @if ($errors->has('title'))
-        <span class="help-block">
-          @foreach ($errors->get('title') as $error)
-            {{ $error }}
-          @endforeach
-        </span>
-      @endif
+    <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
+      {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
+      <span class="help-block">
+        {{ $errors->first('title') }}
+      </span>
     </div>
-    <div class="form-group">
-      {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Content', 'cols' => '50', 'rows' => '10'])!!}
-      @if ($errors->has('content'))
-        <span class="help-block">
-          @foreach ($errors->get('content') as $error)
-            {{ $error }}
-          @endforeach
-        </span>
-      @endif
+    <div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
+      {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Content', 'cols' => '50', 'rows' => '10'])!!}  
+      <span class="help-block">
+        {{ $errors->first('content') }}
+      </span>
     </div>
     {!! Form::submit('Add', ['class' => 'btn btn-success pull-right']) !!}
   {!! Form::close() !!}
