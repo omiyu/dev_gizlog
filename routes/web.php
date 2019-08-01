7c038caa30c08a815db00637abf9ca1390d4c259
omiyu <o1m.hello@icloud.com>
2019-08-01 20:56:33 +0900

    confirmメソッドのルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::group(['prefix' => '/', 'user.', 'namespace' => 'User'], function () {
 
     Route::resource('report', DailyReportController::class);
     Route::get('/question/myPageTop', 'QuestionController@myPageTop')->name('question.myPageTop');
-
+    Route::get('/question/confirm/{id}', 'QuestionController@confirm')->name('question.confirm');
     Route::resource('question', QuestionController::class);
 
 });
