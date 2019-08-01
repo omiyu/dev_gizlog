b83255e8af491bbf9126ae89b031b5935b0b9cdd
omiyu <o1m.hello@icloud.com>
2019-08-01 12:36:24 +0900

    createビューでのカテゴリー表示とformタグをlaravelcollectiveに変更

--- resources/views/user/question/create.blade.php
+++ resources/views/user/question/create.blade.php
@@ -4,24 +4,26 @@
 <h2 class="brand-header">質問投稿</h2>
 <div class="main-wrap">
   <div class="container">
-    <form>
+    {!! Form::open(['route' => 'question.store']) !!}
       <div class="form-group">
         <select name='tag_category_id' class = "form-control selectpicker form-size-small" id="pref_id">
-          <option value="">Select category</option>
-            <option value= ""></option>
+          <option value="0">Select category</option>
+            @foreach ($categories as $category)
+              <option value= "{{ $category->id }}">{{ $category->name }}</option>
+            @endforeach
         </select>
         <span class="help-block"></span>
       </div>
       <div class="form-group">
-        <input class="form-control" placeholder="title" name="title" type="text">
+        {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
         <span class="help-block"></span>
       </div>
       <div class="form-group">
-        <textarea class="form-control" placeholder="Please write down your question here..." name="content" cols="50" rows="10"></textarea>
+        {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Please write down your question here...', 'cols' => '50', 'rows' => '10']) !!}
         <span class="help-block"></span>
       </div>
       <input name="confirm" class="btn btn-success pull-right" type="submit" value="create">
-    </form>
+    {!! Form::close() !!}
   </div>
 </div>
 
