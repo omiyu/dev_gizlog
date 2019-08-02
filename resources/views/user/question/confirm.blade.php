9e8921bb5593bb12030fd5d31693f4b96bdf8938
omiyu <o1m.hello@icloud.com>
2019-08-02 12:04:55 +0900

    confirm.blade.phpにDBから取得したデータ挿入

--- resources/views/user/question/confirm.blade.php
+++ resources/views/user/question/confirm.blade.php
@@ -5,31 +5,31 @@
 <div class="main-wrap">
   <div class="panel panel-success">
     <div class="panel-heading">
-      の質問
+      {{ $question[0]->id }}の質問
     </div>
     <div class="table-responsive">
       <table class="table table-striped table-bordered">
         <tbody>
           <tr>
             <th class="table-column">Title</th>
-            <td class="td-text"></td>
+            <td class="td-text">{{ $question[0]->title }}</td>
           </tr>
           <tr>
             <th class="table-column">Question</th>
-            <td class='td-text'></td>
+            <td class='td-text'>{{ $question[0]->content }}</td>
           </tr>
         </tbody>
       </table>
     </div>
   </div>
   <div class="btn-bottom-wrapper">
-    <form>
+    {!! Form::open(['route' => 'question.index', 'method' => 'GET']) !!}
       <input name="user_id" type="hidden" value="">
       <input name="tag_category_id" type="hidden" value="">
       <input name="title" type="hidden" value="">
       <input name="content" type="hidden" value="">
       <button type="submit" class="btn btn-success"><i class="fa fa-check" aria-hidden="true"></i></button>
-    </form>
+    {!! Form::close() !!}
   </div>
 </div>
 
