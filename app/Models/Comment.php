b2e38e7f6767bf1eb63bec38184cb79debae8760
omiyu <o1m.hello@icloud.com>
2019-08-04 23:59:23 +0900

    questionsとusersテーブルのリレーション設定

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Question;
 
 class Comment extends Model
 {
-    //
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
