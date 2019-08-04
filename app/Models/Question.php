bac48cf5bb0ce89c4d93abc3d742c009d051540c
omiyu <o1m.hello@icloud.com>
2019-08-05 00:00:09 +0900

    questionテーブルとcommentsテーブルのリレーション設定

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\TagCategory;
+use App\Models\User;
+use App\Models\Comment;
 
 class Question extends Model
 {
@@ -22,13 +25,41 @@ class Question extends Model
         'deleted_at',
     ];
 
+    public function tagCategory()
+    {
+        return $this->belongsTo(TagCategory::class);
+    }
+
+    public function comment()
+    {
+        return $this->hasMany(Comment::class, 'question_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
     public function getQuestions($id)
     {
+        //　joinの記述
+        // $sql = $this->join('tag_categories', function ($join) {
+        //                 $join->on('questions.tag_category_id', '=', 'tag_categories.id')
+        //                      ->where('questions.id', $id);
+        //             })
+        //             ->get();
+        // dd($sql);
+
         if (empty($id)) {
+            // $aa = $this->get();
+            // dd($aa[1]->tagCategory);
             return $this->get();
+            // return $this->get();
         }
 
         if($id) {
+            // dd($sql->where('id', $id));
             return $this->where('id', $id)->get();
         }
         
@@ -36,6 +67,7 @@ class Question extends Model
 
     public function getQuestionsByUserId($user_id)
     {
+        // dd( $this->tagCategory );
         return $this->where('user_id', $user_id)->get();
     }
 
